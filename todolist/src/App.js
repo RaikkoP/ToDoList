e6385a0f31b00b9c119774dd562242e49c6424e0
RaikkoP <RaikkoP@users.noreply.github.com>
2023-02-21 20:36:22 +0200

    test

--- todolist/src/App.js
+++ todolist/src/App.js
@@ -1,10 +1,28 @@
-import ToDoListForm from "./Components/ToDoListForm/ToDoListForm.js";
+
+import "./App.css"
+import ToDoItem from "./Components/ToDoItem/ToDoItem.js"
 
 function App() {
+  
+  const exampleData = [
+    {
+      date: new Date(2023,0,10),
+      title: 'Read a book',
+      priority: 'medium'
+    },
+    {
+      date: new Date(2023,0,10),
+      title: 'Walk Grandmother',
+      priority: 'low'
+    }
+  ]
 
-return(
-  <ToDoListForm/>
-)
+  return(
+    <div>
+      <ToDoItem example={exampleData[0]}></ToDoItem>
+      <ToDoItem example={exampleData[1]}></ToDoItem>
+    </div>
+  )
   
 }
 
