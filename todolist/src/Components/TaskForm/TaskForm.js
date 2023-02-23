386cdaab54cf9d1eb931937a080f0fc0c316c94e
RaikkoP <RaikkoP@users.noreply.github.com>
2023-02-23 17:06:33 +0200

    Vormitöötluse osad

--- todolist/src/Components/TaskForm/TaskForm.js
+++ todolist/src/Components/TaskForm/TaskForm.js
@@ -1,22 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import "./TaskForm.css"
 
 const TaskForm = (props) => {
+
+    const [task, setTask] = useState('')
+    const [date, setDate] = useState('')
+    const [priority, setPriority] = useState('')
+
+    const taskChangeHandler = (event) => {
+        setTask(event.target.value)
+        console.log(event.target.value)
+    }
+
+    const dateChangeHandler = (event) => {
+        setDate(event.target.value)
+        console.log(event.target.value)
+    }
+
+    const priorityChangeHandler = (event) => {
+        setPriority(event.target.value)
+        console.log(event.target.value)
+    }
 
     return(
         <form className="box">
             <div>
                 <div>
                     <label>Task</label>
-                    <input type="text"></input>
+                    <input type="text" onChange={taskChangeHandler}></input>
                 </div>
                 <div>
                     <label>Date</label>
-                    <input type="date" min="2023-01-01" max="2025-12-31"></input>
+                    <input type="date" onChange={dateChangeHandler} min="2023-01-01" max="2025-12-31"></input>
                 </div>
                 <div>
                     <label>Priority</label>
-                    <select>
+                    <select onChange={priorityChangeHandler}>
                         <option value= '0'>Low</option>
                         <option value= '1'>Medium</option>
                         <option value= '2'>High</option>
